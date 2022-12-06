dea16d38a240aa58bc67bc8087db6b0b1cb9ee22
wenqi0425 <xiaoc288@gmail.com>
2022-12-06 22:38:46 +0100

    SeedData in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+
+using RazorPageFoodie.Data;
 
 using System;
 using System.Collections.Generic;
@@ -14,7 +17,23 @@ namespace RazorPageFoodie
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+           var host = CreateHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    SeedData.Initialize(services);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occured while attempting to seed the database");
+                }
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
