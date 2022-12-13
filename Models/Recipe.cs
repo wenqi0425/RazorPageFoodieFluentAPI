fb245496053bab22a770ba45b911dd6ed02e30d9
wenqi0425 <xiaoc288@gmail.com>
2022-12-13 21:40:38 +0100

    Id

--- Models/Recipe.cs
+++ Models/Recipe.cs
@@ -8,7 +8,7 @@ namespace RazorPageFoodie.Models
     public class Recipe
     {
         [Key]
-        public int RecipeID { get; set; }
+        public int Id { get; set; }
 
         [Required]
         [Display(Name = "Recipe Name:")]
